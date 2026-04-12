a0ba3b95025fcd15d127efe742fd52653e2d8884
Dark99965 <noah.retter@icloud.com>
2026-04-12 20:38:39 -0300

    changed backround color

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,6 +1,6 @@
 export default function Page({ children }) {
     return (
-        <main style={{fontFamily: "sans-serif", height: "100vh", width: "100vw"}}>
+        <main style={{fontFamily: "sans-serif", backgroundColor: "#0f0e17", color: "#fffffe", height: "100vh", width: "100vw"}}>
             { children }
         </main>
     )
